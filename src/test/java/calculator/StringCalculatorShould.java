7213d479e59aa3b14a502f3988127c8d66663b35
Shrinathgadekar-1025 <shrinath658@gmail.com>
2021-10-17 13:07:15 +0530

    test case 2 completed

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -26,4 +26,9 @@ class StringCalculatorShould {
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
+	
+	@Test
+	public void testTwoNumbers(){
+		assertEquals(3, Calculator.add("1,2"));
+	}
 }
